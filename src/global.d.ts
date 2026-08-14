046548ceaf668b645b9b5c15996d5a58fe9092dc
365Evergreen <paul@365evergreen.com>
2026-08-15 03:33:41 +1000

    styles

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,10 +1,4 @@
 declare module "*.css" ;
 declare module "*.scss";
-declare module '*.module.css' {
- const classes: { [key: string]: string };
- export default classes;
-}
-declare module '*.module.scss' {
- const classes: { [key: string]: string };
- export default classes;
-}
+declare module '*.module.css' 
+declare module '*.module.scss'
