17517c3f423d17a328b41b65d1bbd81673baa0b9
365Evergreen <paul@365evergreen.com>
2026-08-28 19:34:26 +1000

    Remove merge conflict markers and clean up AppRouter component

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import AppShell from '../layouts/AppShell/AppShell';
 import HomePage from '../pages/HomePage/HomePage';
@@ -17,26 +16,4 @@ export default function AppRouter() {
            
         </BrowserRouter>
     );
-=======
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AppShell from "../layouts/AppShell";
-import HomePage from '../pages/HomePage/HomePage'
-import AdminPage from "../pages/AdminPage/AdminPage";
-import BlogArchivePage from "../pages/BlogArchivePage/BlogArchivePage";
-import SinglePostPage from '../pages/SinglePostPage/SinglePostPage'
-
-export default function AppRouter() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppShell />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/blog" element={<BlogArchivePage />} />
-          <Route path="/blog/:slug" element={<SinglePostPage />} />
-          <Route path="/admin" element={<AdminPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
->>>>>>> 59aabae71b4359f0bf4cefe3869c50fefbbb9dab
 }
