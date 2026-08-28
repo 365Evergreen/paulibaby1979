42b9d8f732201b1c67344b7e06c4d2efd9bf0241
365Evergreen <paul@365evergreen.com>
2026-08-28 21:00:16 +1000

    pauli

--- src/layouts/AppShell/AppShell.tsx
+++ src/layouts/AppShell/AppShell.tsx
@@ -8,6 +8,7 @@ export function AppShell(){
     return (
         <div className="app-shell">
             <SiteHeader />
+            <div id="pauli">Paulibaby</div>
             <main className={styles.mainContainer}>
              <Outlet/>            
             </main>
