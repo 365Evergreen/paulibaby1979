2918db331c9dd377e1c5d2dc52c3b1cb7d34d71e
365Evergreen <paul@365evergreen.com>
2026-09-03 03:11:30 +1000

    style: Update styles for LatestPosts, SiteHeader, SiteNav, and index.css for improved layout and consistency

--- src/components/SiteNav/SiteNav.tsx
+++ src/components/SiteNav/SiteNav.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import styles from './SiteNav.module.css'
+import { NavLink } from "react-router-dom"; // Make sure this is NavLink, not Link
+import styles from './SiteNav.module.css';
+
 const SiteNav: React.FC = () => {
  return (
    <nav className={styles.navContainer}>
      <ul className={styles.navLinks}>
-       <li><Link to="/">Home</Link></li>
-       <li><Link to="/media-library">Media library</Link></li>
-       <li><Link to="/admin">Editor</Link></li>
-       <li><Link to="/contact">Contact</Link></li>
+       <li><NavLink to="/">Home</NavLink></li>
+       <li><NavLink to="/media-library">Media library</NavLink></li>
+       <li><NavLink to="/admin">Editor</NavLink></li>
+       <li><NavLink to="/contact">Contact</NavLink></li>
      </ul>
    </nav>
  );
 };
 
- 
 export default SiteNav;
